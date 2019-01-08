ccb9cd625bd46f90f865717a39cd037086531f05
meatbags <jxburrow@gmail.com>
2019-01-08 17:50:26 +1100

    scaffolding, rough style, basic functionality

--- js/carousel.js
+++ js/carousel.js
@@ -5,7 +5,43 @@
 
 class Carousel {
   constructor() {
+    // get targets
+    this.target = {
+      buttonLeft: document.querySelector('#carousel-button-left'),
+      buttonRight: document.querySelector('#carousel-button-right'),
+      indexCurrent: document.querySelector('#carousel-slide-index-current'),
+      indexMax: document.querySelector('#carousel-slide-index-max'),
+      slider: document.querySelector('#carousel-slider'),
+      slides: document.querySelectorAll('.carousel__slide'),
+    };
+    this.currentIndex = 0;
+    this.maxIndex = this.target.slides.length - 1;
 
+    this.target.buttonLeft.addEventListener('click', () => { this.goToIndex(this.currentIndex - 1); });
+    this.target.buttonRight.addEventListener('click', () => { this.goToIndex(this.currentIndex + 1); });
+
+    this.setIndex();
+  }
+
+  goToIndex(index) {
+    const clamped = Math.min(this.maxIndex, Math.max(0, index));
+    if (clamped != this.currentIndex) {
+      this.currentIndex = clamped;
+      this.setIndex();
+    }
+  }
+
+  setIndex() {
+    const i = this.currentIndex + 1;
+    const j = this.maxIndex + 1;
+    this.target.indexCurrent.innerHTML = `${i > 9 ? i : '0' + i}`;
+    this.target.indexMax.innerHTML = `${j > 9 ? j : '0' + j}`;
+  }
+
+  resize() {
+    this.target.slider.classList.add('transition-lock');
+
+    this.target.slider.classList.remove('transition-lock');
   }
 }
 
--- js/parallax.js
+++ js/parallax.js
@@ -4,7 +4,25 @@
  **/
 
 class Parallax {
+  constructor() {
+    // targets
+    this.target = {
+      scrollHint: document.querySelector('.scroll-hint'),
+    };
 
+    // bind scroll
+    window.addEventListener('scroll', () => { this.onScroll(); });
+  }
+
+  onScroll() {
+    const y = document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (y == 0) {
+      this.target.scrollHint.classList.remove('hidden');
+    } else {
+      this.target.scrollHint.classList.add('hidden');
+    }
+  }
 }
 
 export { Parallax };
